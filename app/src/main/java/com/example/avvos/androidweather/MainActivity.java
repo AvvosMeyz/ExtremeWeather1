837f821d5ca9fa0bc02c493fd73a43d2769549ac
Avvos <hmztkn@gmail.com>
2017-05-18 20:49:19 +0300

    Added Tab

--- app/src/main/java/com/example/avvos/androidweather/MainActivity.java
+++ app/src/main/java/com/example/avvos/androidweather/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.avvos.androidweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, TabActivity.class);
+        startActivity(intent);
     }
 }
